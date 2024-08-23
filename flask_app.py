64cd1786f5be1eecf633867dab7c36cc06855579
tbui468 <thomasthienbui@gmail.com>
2024-08-22 17:43:55 -0700

    initial commit

---
+++ flask_app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for, g, session, flash
+import os
+from markupsafe import Markup
+import sqlite3
+from werkzeug.security import check_password_hash
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect("blog.db")
+    return g.db
+
+def index():
+    return render_template("index.html")
+
+def blog_posts():
+    db = get_db()
+    results = db.execute("SELECT rowid, title, content FROM posts ORDER BY created DESC").fetchall()
+    data = []
+    for r in results:
+        data.append({ "pid": r[0], "title": r[1], "content": r[2] })
+    return render_template("posts.html", post_data=data)
+
+def view():
+    pid = request.args.get("pid")
+    db = get_db()
+    result = db.execute("SELECT rowid, title, content FROM posts WHERE rowid=?", (pid)).fetchone()
+    return render_template("view.html", data={ "pid": result[0], "title": result[1], "content": result[2] })
+
+def create():
+    user_id = session.get("user_id")
+    if user_id == None:
+        return redirect(url_for("login"))
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        #include user_id with INSERT
+        #fill database with new title and content
+        return redirect(url_for("posts"))
+
+    return render_template("create.html")
+
+def logout():
+    if request.method == 'POST':
+        session.clear()
+    return render_template("index.html")
+    
+
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        query = "SELECT password, rowid FROM users WHERE username=?"
+        #hashed_password = db.execute(query, (username,)).fetchone()[0]
+        row = db.execute(query, (username,)).fetchone()
+        if not row == None:
+            hashed_password = row[0]
+            rowid = row[1]
+            if check_password_hash(hashed_password, password):
+                session.clear()
+                session['user_id'] = rowid
+                flash("Successfully logged in")
+                return redirect(url_for("index"))
+
+        flash("Username or password invalid")
+        return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+app = Flask(__name__, template_folder=os.getcwd(), static_folder=os.getcwd())
+app.config.from_mapping(SECRET_KEY='dev')
+app.add_url_rule("/", "index", index)
+app.add_url_rule("/posts", "posts", blog_posts)
+app.add_url_rule("/posts/view", "view", view)
+app.add_url_rule("/posts/create", "create", create, methods=['GET', 'POST'])
+app.add_url_rule("/users/login", "login", login, methods=['GET', 'POST'])
+app.add_url_rule("/users/logout", "logout", logout, methods=['POST'])
+
+#app.run()
+
---
+++ init_db.py
@@ -0,0 +1,86 @@
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+#users table (username, password)
+#adding a new column to posts author_id
+#connecting tables using a (inner) join
+#hashing passwords with weurkzueg (spelling ?)
+#logging in with sessions (including a secret key to encrypt session data)
+#flashing messages (which require sessions)
+#loggin out
+#requiring login for certain routes
+
+#exercise
+#adding an edit button for a post
+
+
+db = sqlite3.connect("blog.db")
+
+#drop tables
+db.execute("DROP TABLE IF EXISTS posts")
+db.execute("DROP TABLE IF EXISTS users")
+
+#create tables
+create_table = "CREATE TABLE posts(author_id INTEGER NOT NULL, title TEXT NOT NULL, content TEXT NOT NULL, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+db.execute(create_table)
+create_users_table = "CREATE TABLE users (username TEXT NOT NULL, password TEXT NOT NULL, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+db.execute(create_users_table)
+
+#insert rows into tables
+insert_user_stmt = "INSERT INTO users (username, password) VALUES (?, ?)"
+db.execute(insert_user_stmt, ("thomas", generate_password_hash("password", method='pbkdf2')))
+db.execute(insert_user_stmt, ("kate", generate_password_hash("password", method='pbkdf2')))
+
+author_id = db.execute("SELECT rowid FROM users WHERE username=?", ('thomas',)).fetchone()[0]
+insert_stmt = "INSERT INTO posts (author_id, title, content) VALUES (?, ?, ?)"
+db.execute(insert_stmt, (author_id, 'title 1', 'content 1'))
+db.execute(insert_stmt, (author_id, 'title 2', 'content 2'))
+
+author_id = db.execute("SELECT rowid FROM users WHERE username=?", ('kate',)).fetchone()[0]
+db.execute(insert_stmt, (author_id, 'title 3', 'content 3'))
+db.execute(insert_stmt, (author_id, 'title 4', 'content 4'))
+
+results = db.execute("SELECT author_id, title, content FROM posts").fetchall()
+for r in results:
+    print(r)
+
+results = db.execute("SELECT rowid, username, password FROM users").fetchall()
+for r in results:
+    print(r)
+
+query = "SELECT username, password, title, content FROM posts JOIN users ON users.rowid = author_id"
+results = db.execute(query).fetchall()
+for r in results:
+    print(r)
+
+db.commit()
+db.close()
+
+'''
+db = sqlite3.connect("blog.db")
+
+db.execute("DROP TABLE IF EXISTS posts")
+db.execute("DROP TABLE IF EXISTS users")
+
+create_table = "CREATE TABLE posts(author_id INTEGER, title TEXT NOT NULL, content TEXT NOT NULL, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+db.execute(create_table)
+
+create_users_table = "CREATE TABLE users(username TEXT NOT NULL, password TEXT NOT NULL, created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+db.execute(create_users_table)
+
+insert_stmt = "INSERT INTO users (username, password) VALUES (?, ?)"
+db.execute(insert_stmt, ("test", "password"))
+
+query_author = db.execute("SELECT rowid from users WHERE username=?", ("test",)).fetchone()
+insert_stmt = "INSERT INTO posts (author_id, title, content) VALUES (?, ?, ?)"
+db.execute(insert_stmt, (query_author[0], 'title 1', 'content 2'))
+
+results = db.execute("SELECT username, title, content FROM posts JOIN users ON author_id = users.rowid").fetchall()
+for r in results:
+    title = r[0]
+    content = r[1]
+    print(r)
+
+db.commit()
+db.close()
+'''
